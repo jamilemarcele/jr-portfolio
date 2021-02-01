41603d23adc0d01d9175e2438112ed386e3d6e54
Jamile Radloff <jamilemarcele@gmail.com>
2021-02-01 14:37:59 +0100

    fixing layout

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
     new Typed('#banner-typed-text', {
-        strings: ["Hey, I'm <span id='curly-font'>Jamile</span>"],
+        strings: ["Hi, I'm <span id='curly-font'>Jamile</span>"],
         typeSpeed: 150,
         loop: false
     });
